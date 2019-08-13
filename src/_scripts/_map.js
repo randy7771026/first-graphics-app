dcb27c071b985a748f85d957bb9cb9a43594015d
Randall Baxley <randy7771026@gmail.com>
2019-08-13 17:02:10 -0500

    corner paragraph

--- src/_scripts/_map.js
+++ src/_scripts/_map.js
@@ -1,5 +1,8 @@
-var map = L.map('map');
+var map = L.map('map', {
+    scrollWheelZoom: false
+});
 var sat = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibGF0aW1lcyIsImEiOiJjanJmNjg4ZzYweGtvNDNxa2ZpZ2lma3Z4In0.g0lYVIEs9Y5QcUOhXactHA');
+ minZoom: 9
 sat.addTo(map);
 map.setView([33.983265, -118.306799], 18);
 
